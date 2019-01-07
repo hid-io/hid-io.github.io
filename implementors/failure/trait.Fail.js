892292273e53c31f2b7a6d0ab8a62ed374e5d08e
Jacob Alexander <haata@kiibohd.com>
2019-01-07 23:05:40 +0000

    rebuild pages at dc5db0d

--- implementors/failure/trait.Fail.js
+++ implementors/failure/trait.Fail.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["failure"] = [];
+implementors["hidapi"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"hidapi/enum.HidError.html\" title=\"enum hidapi::HidError\">HidError</a>",synthetic:false,types:["hidapi::error::HidError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
